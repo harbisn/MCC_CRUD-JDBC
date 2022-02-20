88913b8b2b28aad64ab886ed506a757515dbbd06
Harbi <95677212+harbisn@users.noreply.github.com>
2022-02-20 19:57:14 +0700

    Update LocationCRUD.java

--- MySql_JDBC/src/tools/LocationCRUD.java
+++ MySql_JDBC/src/tools/LocationCRUD.java
@@ -11,55 +11,48 @@ public class LocationCRUD implements CRUD {
     DbConnection connection = new DbConnection();
     LocationDAO ldao = new LocationDAO(connection.getConncetion());
     Scanner scan = new Scanner(System.in);
-
+    List<String> locationListsStr = new ArrayList<>();
+    List<Integer> locationListsInt = new ArrayList<>();
+    
+     public void ListOfLocations(){
+        ldao.getAll().forEach((i) -> locationListsStr.add(i.getCountryId() + i.getStreetAddress()));
+        ldao.getAll().forEach((i) -> locationListsInt.add(i.getLocationId()));
+     }
+    
     @Override
     public void show() {
         System.out.println("id -- street address -- postal code -- city -- state province -- country id");
         for (Location location : ldao.getAll()) {
-            System.out.print(location.getLocationId() + " -- ");
-            System.out.print(location.getStreetAddress() + " -- ");
-            System.out.print(location.getPostalCode() + " -- ");
-            System.out.print(location.getCity() + " -- ");
-            System.out.print(location.getStateProvince() + " -- ");
-            System.out.println(location.getCountryId());
+            System.out.println(location.getLocationId() + " -- " + location.getStreetAddress() + " -- " 
+                    + location.getPostalCode() + " -- " + location.getCity() + " -- " + location.getStateProvince() + " -- " 
+                    + location.getCountryId());
         }
     }
 
     @Override
     public void insert() {
         System.out.print("Insert new ID: ");
-       int locationId_Insert = scan.nextInt();
-       scan.nextLine();       
-       System.out.print("Insert new Street Address: ");
-       String streetAddress_Insert = scan.nextLine();               
-       System.out.print("Insert new Postal Code: ");
-       String postalCode_Insert = scan.nextLine();               
-       System.out.print("Insert new City: ");
-       String city_Insert = scan.nextLine();               
-       System.out.print("Insert new State Province: ");
-       String stateProvince_Insert = scan.nextLine();              
-       System.out.print("insert new Country ID: ");
-       String countryId_Insert = scan.nextLine();
-       
-       
-       List<String> locationsInsertSACid = new ArrayList<>();
-       for (Location location : ldao.getAll()){
-           locationsInsertSACid.add(location.getCountryId());
-           locationsInsertSACid.add(location.getStreetAddress());
-       }
-       
-       List<Integer> locationsInsertID = new ArrayList<>();
-       for (Location location : ldao.getAll()){
-           locationsInsertID.add(location.getLocationId());
-       }
+        int locationId_Insert = scan.nextInt();
+        scan.nextLine();       
+        System.out.print("Insert new Street Address: ");
+        String streetAddress_Insert = scan.nextLine();               
+        System.out.print("Insert new Postal Code: ");
+        String postalCode_Insert = scan.nextLine();               
+        System.out.print("Insert new City: ");
+        String city_Insert = scan.nextLine();               
+        System.out.print("Insert new State Province: ");
+        String stateProvince_Insert = scan.nextLine();              
+        System.out.print("insert new Country ID: ");
+        String countryId_Insert = scan.nextLine();
+        ListOfLocations();
 
-       boolean validasiInsert_Cid = locationsInsertSACid.stream().anyMatch(countryId_Insert::contains);
-       boolean validasiInsert_SA = locationsInsertSACid.stream().anyMatch(streetAddress_Insert::contains);
-       boolean validasiInsert_ID = locationsInsertID.stream().anyMatch(n -> (n == locationId_Insert));
+        boolean validasiInsert_Cid = locationListsStr.stream().anyMatch(countryId_Insert::contains);
+        boolean validasiInsert_SA = locationListsStr.stream().anyMatch(streetAddress_Insert::contains);
+        boolean validasiInsert_ID = locationListsInt.stream().anyMatch(n -> (n == locationId_Insert));
        
-       if (validasiInsert_Cid == false){
+        if (validasiInsert_Cid == false){
            System.out.println("country with country ID of " + countryId_Insert + " do not exists in the database");
-       } else {
+        } else {
            if (validasiInsert_SA == false && validasiInsert_ID == false){
            Location locationInsert = new Location(locationId_Insert, streetAddress_Insert, postalCode_Insert, 
            city_Insert, stateProvince_Insert, countryId_Insert);
@@ -78,28 +71,23 @@ public class LocationCRUD implements CRUD {
     @Override
     public void update() {
         System.out.print("Insert ID: ");
-       int locationId_Update = scan.nextInt();
-       scan.nextLine();
-       System.out.println("Current data:\n" + ldao.getById(locationId_Update));
-       System.out.print("new Street Address: ");
-       String streetAddress_Update = scan.nextLine();               
-       System.out.print("new Postal Code: ");
-       String postalCode_Update = scan.nextLine();               
-       System.out.print("new City: ");
-       String city_Update = scan.nextLine();               
-       System.out.print("new State Province: ");
-       String stateProvince_Update = scan.nextLine();              
-       System.out.print("new Country ID: ");
-       String countryId_Update = scan.nextLine();
-       
-       List<String> locationsUpdateSACid = new ArrayList<>();
-       for (Location location : ldao.getAll()){
-           locationsUpdateSACid.add(location.getCountryId());
-           locationsUpdateSACid.add(location.getStreetAddress());
-       }
-
-       boolean validasiUpdate_Cid = locationsUpdateSACid.stream().anyMatch(countryId_Update::contains);
-       boolean validasiUpdate_SA = locationsUpdateSACid.stream().anyMatch(streetAddress_Update::contains);
+        int locationId_Update = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Current data:\n" + ldao.getById(locationId_Update));
+        System.out.print("new Street Address: ");
+        String streetAddress_Update = scan.nextLine();               
+        System.out.print("new Postal Code: ");
+        String postalCode_Update = scan.nextLine();               
+        System.out.print("new City: ");
+        String city_Update = scan.nextLine();               
+        System.out.print("new State Province: ");
+        String stateProvince_Update = scan.nextLine();              
+        System.out.print("new Country ID: ");
+        String countryId_Update = scan.nextLine();
+        
+        ListOfLocations();
+        boolean validasiUpdate_Cid = locationListsStr.stream().anyMatch(countryId_Update::contains);
+        boolean validasiUpdate_SA = locationListsStr.stream().anyMatch(streetAddress_Update::contains);
        
        if (validasiUpdate_Cid == false){
            System.out.println("Country ID do not exists in the database");
@@ -117,29 +105,24 @@ public class LocationCRUD implements CRUD {
 
     @Override
     public void delete() {
-         System.out.print("Select location ID: ");
-       int locationId_Delete = scan.nextInt();
-       scan.nextLine();
-       System.out.println("Target data to delete:\n" + ldao.getById(locationId_Delete));
-       System.out.print("Retype location ID to delete: ");
-       int confirmDeletion = scan.nextInt();
-       scan.nextLine();
-       
-       List<Integer> locationsDeleteID = new ArrayList<>();
-       for (Location location : ldao.getAll()){
-           locationsDeleteID.add(location.getLocationId());
-       }
+        System.out.print("Select location ID: ");
+        int locationId_Delete = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Target data to delete:\n" + ldao.getById(locationId_Delete));
+        System.out.print("Retype location ID to delete: ");
+        int confirmDeletion = scan.nextInt();
+        scan.nextLine();
+        
+        ListOfLocations();
+        boolean validasiDelete_ID = locationListsInt.stream().anyMatch(n -> (n == locationId_Delete));
 
-       boolean validasiDelete_ID = locationsDeleteID.stream().anyMatch(n -> (n == locationId_Delete));
-       
-       if (validasiDelete_ID == true){
+        if (validasiDelete_ID == true){
            ldao.delete(confirmDeletion);
            System.out.println("the data successfully deleted");
-       } else {
+        } else {
            System.out.println("the data is not exist in the database");
-       }
-    }
-            
+        }
+    }           
 }
 
 
